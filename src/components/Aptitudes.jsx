45ebb38ab8b294be47087591a89e4c46f6ada0fa
Nahuel <Nahuelpardo3@gmail.com>
2023-07-27 20:17:42 -0300

    fix

--- src/components/Aptitudes.jsx
+++ src/components/Aptitudes.jsx
@@ -1,3 +1,15 @@
+import css3 from '../img/logos/css3.png'
+import github from '../img/logos/github.png'
+import gulp from '../img/logos/gulp.png'
+import html5 from '../img/logos/html5.png'
+import js from '../img/logos/js.png'
+import mysql from '../img/logos/mysql.png'
+import php from '../img/logos/php.png'
+import reactjs from '../img/logos/reactjs.png'
+import sass from '../img/logos/sass.png'
+import tailwind from '../img/logos/tailwind.png'
+
+
 const Aptitudes = () => {
   return (
     <>
@@ -7,52 +19,52 @@ const Aptitudes = () => {
           <div className="grid grid-cols-3 md:grid-cols-6 gap-10 mt-10">
 
             <div className="flex flex-col gap-1 h-20 w-20 items-center justify-center">
-              <img src="src/img/logos/reactjs.png" alt="logo react" className="transition-transform duration-300 transform-gpu hover:scale-110"/>
+              <img src={reactjs} alt="logo react" className="transition-transform duration-300 transform-gpu hover:scale-110"/>
               <p className="font-semibold">React</p>
             </div>
 
             <div className="flex flex-col gap-1 h-20 w-20 items-center justify-center">
-              <img src="src/img/logos/tailwind.png" alt="logo tailwind" className="transition-transform duration-300 transform-gpu hover:scale-110" />
+              <img src={tailwind} alt="logo tailwind" className="transition-transform duration-300 transform-gpu hover:scale-110" />
               <p className="font-semibold">TailwindCSS</p>
             </div>
 
             <div className="flex flex-col gap-1 h-20 w-20 items-center justify-center">
-              <img src="src/img/logos/css3.png" alt="logo css3" className="transition-transform duration-300 transform-gpu hover:scale-110"/>
+              <img src={css3} alt="logo css3" className="transition-transform duration-300 transform-gpu hover:scale-110"/>
               <p className="font-semibold">CSS3</p>
             </div>
 
             <div className="flex flex-col gap-1 h-20 w-20 items-center justify-center">
-              <img src="src/img/logos/html5.png" alt="logo html 5" className="transition-transform duration-300 transform-gpu hover:scale-110"/>
+              <img src={html5} alt="logo html 5" className="transition-transform duration-300 transform-gpu hover:scale-110"/>
               <p className="font-semibold">HTML 5</p>
             </div>
 
             <div className="flex flex-col gap-1 h-20 w-20 items-center justify-center">
-              <img src="src/img/logos/js.png" alt="logo JavaScript" className="transition-transform duration-300 transform-gpu hover:scale-110"/>
+              <img src={js} alt="logo JavaScript" className="transition-transform duration-300 transform-gpu hover:scale-110"/>
               <p className="font-semibold">JavaScript</p>
             </div>
 
             <div className="flex flex-col gap-1 h-20 w-20 items-center justify-center">
-              <img src="src/img/logos/github.png" alt="logo github" className="transition-transform duration-300 transform-gpu hover:scale-110"/>
+              <img src={github} alt="logo github" className="transition-transform duration-300 transform-gpu hover:scale-110"/>
               <p className="font-semibold">Github</p>
             </div>
 
             <div className="flex flex-col gap-1 h-20 w-20 items-center justify-center">
-              <img src="src/img/logos/mysql.png" alt="logo mysql" className="transition-transform duration-300 transform-gpu hover:scale-110"/>
+              <img src={mysql} alt="logo mysql" className="transition-transform duration-300 transform-gpu hover:scale-110"/>
               <p className="font-semibold">MySQL</p>
             </div>
 
             <div className="flex flex-col gap-1 h-20 w-20 items-center justify-center">
-              <img src="src/img/logos/php.png" alt="logo php" className="transition-transform duration-300 transform-gpu hover:scale-110"/>
+              <img src={php} alt="logo php" className="transition-transform duration-300 transform-gpu hover:scale-110"/>
               <p className="font-semibold">PHP</p>
             </div>
 
             <div className="flex flex-col gap-1 h-20 w-20 items-center justify-center">
-              <img src="src/img/logos/gulp.png" alt="logo gulp" className="transition-transform duration-300 transform-gpu hover:scale-110" />
+              <img src={gulp} alt="logo gulp" className="transition-transform duration-300 transform-gpu hover:scale-110" />
               <p className="font-semibold">Gulp</p>
             </div>
 
             <div className="flex flex-col gap-1 h-20 w-20 items-center justify-center">
-              <img src="src/img/logos/sass.png" alt="logo sass" className="transition-transform duration-300 transform-gpu hover:scale-110"/>
+              <img src={sass} alt="logo sass" className="transition-transform duration-300 transform-gpu hover:scale-110"/>
               <p className="font-semibold">SASS</p>
             </div>
 
--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,3 +1,9 @@
+import linkedin from '../img/linkedin.png'
+import github from '../img/github.png'
+import facebook from '../img/facebook.png'
+import twitter from '../img/twitter.png'
+
+
 const Contacto = () => {
   return (
     <>
@@ -12,10 +18,10 @@ const Contacto = () => {
           </div>
 
           <div className="flex justify-around items-center">
-            <a href="https://github.com/NahuPardo" className="w-9 transition-transform duration-300 transform-gpu hover:scale-110"><img src="./src/img/github.png" alt="icono github" /></a>
-            <a href="https://www.linkedin.com/in/nahuelpardo/" className="w-9 transition-transform duration-300 transform-gpu hover:scale-110"><img src="./src/img/linkedin.png" alt="icono linkedin" /></a>
-            <a href="https://twitter.com/nahupardo_" className="w-9 transition-transform duration-300 transform-gpu hover:scale-110"><img src="/src/img/twitter.png" alt="icono twitter" /></a>
-            <a href="https://www.facebook.com/nahuu.pardo" className="w-9 transition-transform duration-300 transform-gpu hover:scale-110"><img src="/src/img/facebook.png" alt="icono facebook" /></a>
+            <a href="https://github.com/NahuPardo" className="w-9 transition-transform duration-300 transform-gpu hover:scale-110"><img src={github} alt="icono github" /></a>
+            <a href="https://www.linkedin.com/in/nahuelpardo/" className="w-9 transition-transform duration-300 transform-gpu hover:scale-110"><img src={linkedin} alt="icono linkedin" /></a>
+            <a href="https://twitter.com/nahupardo_" className="w-9 transition-transform duration-300 transform-gpu hover:scale-110"><img src={twitter} alt="icono twitter" /></a>
+            <a href="https://www.facebook.com/nahuu.pardo" className="w-9 transition-transform duration-300 transform-gpu hover:scale-110"><img src={facebook} alt="icono facebook" /></a>
           </div>
 
         </div>
