fd9eff894be61fff306263810125511276e85d06
Nahuel <Nahuelpardo3@gmail.com>
2023-07-28 11:19:03 -0300

    update

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -7,6 +7,8 @@ import festival from '../img/proyectos/festival.jpg'
 import freelancer from '../img/proyectos/freeLancer.jpg'
 import frontStore from '../img/proyectos/frontEndStore.jpg'
 import pacientesVet from '../img/proyectos/pacientes-vet.jpg'
+import crmClientes from '../img/proyectos/crm-clientes.jpg'
+import portfolio from '../img/proyectos/portfolio.jpg'
 
 const Proyectos = () => {
   const responsive = {
@@ -56,6 +58,27 @@ const Proyectos = () => {
               </div>
                 <img src={controlDeGastos} alt="Proyecto Control de gastos" className=" md:m-2 w-auto xl:h-80 order-1"/>
               </article>
+
+            <article className="flex flex-col gap-2 mt-10 ">
+              <div className="flex flex-col order-2 text-xl justify-between mx-3">
+                <h1 className="font-semibold text-2xl mb-4">CRM Clientes - React Router DOM | Styled Components | Vite | JSON SERVER </h1>
+                <div className="flex flex-row gap-5 my-3 text-xl">
+                  <a href="https://github.com/NahuPardo/Crm-react" className="bg-gradient-to-r from-blue-600 to-blue-800 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  hover:from-blue-600 hover:to-blue-700 duration-300 ...  rounded-md text-white p-2">Repositorio</a>
+                </div>
+              </div>
+                <img src={crmClientes} alt="Proyecto Crm clientes" className=" md:m-2 w-auto xl:h-80 order-1"/>
+              </article>
+              
+            <article className="flex flex-col gap-2 mt-10 ">
+              <div className="flex flex-col order-2 text-xl justify-between mx-3">
+                <h1 className="font-semibold text-2xl mb-4">Portafolio - React | TailwindCSS | Vite </h1>
+                <div className="flex flex-row gap-5 my-3 text-xl">
+                  <a href="https://github.com/NahuPardo/portfolio" className="bg-gradient-to-r from-blue-600 to-blue-800 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  hover:from-blue-600 hover:to-blue-700 duration-300 ...  rounded-md text-white p-2">Repositorio</a>
+                </div>
+              </div>
+                <img src={portfolio} alt="Proyecto Portafolio" className=" md:m-2 w-auto xl:h-80 order-1"/>
+              </article>
+
             <article className="flex flex-col gap-2 mt-10 ">
             <div className="flex flex-col order-2 text-xl justify-between mx-3">
               <h1 className="font-semibold text-2xl mb-4">Administrador Pacientes - React | Tailwind CSS</h1>
